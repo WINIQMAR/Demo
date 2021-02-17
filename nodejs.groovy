83f0f1273e483c3afe753a215907ff2d302a8c43
WINIQMAR <38892178+WINIQMAR@users.noreply.github.com>
2021-02-17 17:19:32 +0530

    Update nodejs.groovy

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
         git('https://github.com/WINIQMAR/Demo/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('vinaykumarnomula')
+            node / gitConfigEmail('winiqmar@gmail.com')
         }
     }
     triggers {
