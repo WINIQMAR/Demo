98af514c8a49b10b38b2b7dc1b4b4cb421ff231d
WINIQMAR <38892178+WINIQMAR@users.noreply.github.com>
2021-03-01 21:49:44 +0530

    Update Sample1.java

--- Sample/Sample1.java
+++ Sample/Sample1.java
@@ -1,10 +1,9 @@
-Public class Sample1 {
+class Sample1 {
 
 public static void main(String[] args) 
  {
   
-    System.out.println("Its a Sample1");
-    System.out.println("Execute");
-    System.out.println("commit");
+    System.out.println("Sample1");
+    System.out.println("Execute inside the folders");
 
  }
