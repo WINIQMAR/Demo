fa8531ba45411bd5756c7c29d1ca6f74b012e6a8
wini <winiqmar@gmail.com>
2021-03-01 21:06:51 +0530

    Commit

---
+++ Sample.java
@@ -0,0 +1,12 @@
+Public class Sample {
+
+public static void main(String[] args) 
+ {
+  
+    System.out.println("Its a sample");
+    System.out.println("Execute");
+    System.out.println("commit");
+
+ }
+
+}
