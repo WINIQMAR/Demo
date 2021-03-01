9cd692a0791f1f440acb1c5816d076a48cd35017
WINIQMAR <38892178+WINIQMAR@users.noreply.github.com>
2021-03-01 21:42:57 +0530

    Update Simple1.java

--- Simple1.java
+++ Simple1.java
@@ -1,7 +1,11 @@
-Public class Simple1 {  
-public static void main(String args[]){  
- System.out.println("Hello Java");  
-System.out.println("Its a Sample1");
-System.out.println("Execute");
-System.out.println("commit");
-    }
+Public class Simple1 {
+
+public static void main(String[] args) 
+ {
+  
+    System.out.println("Simple1");
+    System.out.println("Execute");
+
+ }
+
+}
