1c6f9966396a70bf71b3c4f522f008d61d9ac53d
WINIQMAR <38892178+WINIQMAR@users.noreply.github.com>
2021-03-01 21:35:08 +0530

    Update Sample1.java

--- Sample/Sample1.java
+++ Sample/Sample1.java
@@ -1,10 +1,10 @@
 Public class Sample1 {
+
 public static void main(String[] args) 
  {
+  
     System.out.println("Its a Sample1");
     System.out.println("Execute");
     System.out.println("commit");
 
  }
-
-}
