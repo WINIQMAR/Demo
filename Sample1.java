166050baee5153c960128d24239083c1b7c00baa
WINIQMAR <38892178+WINIQMAR@users.noreply.github.com>
2021-03-01 21:40:22 +0530

    Update and rename Sample1.java to Simple1.java

--- Sample1.java
+++
@@ -1,8 +0,0 @@
-Public class Simple1 {  
-public static void main(String args[]){  
-     System.out.println("Hello Java");  
-      
-  System.out.println("Its a Sample1");
-    System.out.println("Execute");
-    System.out.println("commit");
-    }
---
+++ Simple1.java
@@ -0,0 +1,7 @@
+Public class Simple1 {  
+public static void main(String args[]){  
+ System.out.println("Hello Java");  
+System.out.println("Its a Sample1");
+System.out.println("Execute");
+System.out.println("commit");
+    }
